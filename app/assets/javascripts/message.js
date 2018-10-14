751607bc2856c42b3638067e3dc9bf72a7376d40
Rick-32 <okano.0407@gmail.com>
2018-10-14 13:04:17 +0900

    Edit Save comments with asynchronous function

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('#new-message').submit(function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    console.log(url);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
+});
