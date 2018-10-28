e016329e076d8eba70bbdbfb3cb5d00b2c873f7b
Rick-32 <okano.0407@gmail.com>
2018-10-28 17:51:29 +0900

    Fix from Github Server failure

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,15 +4,21 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json { @new_messages = @messages.where('id > ?', params[:id]) }
+    end
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+        format.html { redirect_to group_messages_path(@group) }
         format.json
       end
+
+      flash[:notice] = 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
