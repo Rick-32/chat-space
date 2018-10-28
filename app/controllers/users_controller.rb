f63766431a67ae938e7e5ce8e042b955bd8a7898
Rick-32 <okano.0407@gmail.com>
2018-10-28 17:59:10 +0900

    Edit where method/ Fix from pull request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(name: current_user.name).limit(20)
+def index
+    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%", current_user).limit(20)
     respond_to do |format|
       format.html
       format.json
