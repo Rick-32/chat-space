5d576a7d8ed3eb1632a6ae3a7d79a220a8154771
Rick-32 <okano.0407@gmail.com>
2018-12-07 21:22:17 +0900

    edit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,88 +1,88 @@
-// $(document).on('turbolinks:load', function () {
-//   function buildHTML(message) {
+$(document).on('turbolinks:load', function () {
+  function buildHTML(message) {
 
-//     var add_image = (message.image) ? `<image class='message--image' src="${message.image}">` : '';
+    var add_image = (message.image) ? `<image class='message--image' src="${message.image}">` : '';
 
-//     var html =
-//      `<div class='chat-main__body--list--message' data-message-id = "${ message.id }">
-//        <div class='chat-main__body--list--message--name'>
-//          ${message.user_name}
-//         </div>
-//         <div class='chat-main__body--list--message--time'>
-//          ${message.created_at}
-//         </div>
-//         <div class='chat-main__body--list--message--text'>
-//          ${message.content}
-//         </div>
-//          ${add_image}
-//       </div>`;
+    var html =
+     `<div class='chat-main__body--list--message' data-message-id = "${ message.id }">
+       <div class='chat-main__body--list--message--name'>
+         ${message.user_name}
+        </div>
+        <div class='chat-main__body--list--message--time'>
+         ${message.created_at}
+        </div>
+        <div class='chat-main__body--list--message--text'>
+         ${message.content}
+        </div>
+         ${add_image}
+      </div>`;
 
-//     return html;
-// };
+    return html;
+};
 
-//   $('#new-message').submit(function (e) {
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action');
-//     var href = window.location.href;
+  $('#new-message').submit(function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    var href = window.location.href;
 
-//     $.ajax({
-//       url: href,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
 
-//     .done(function (message) {
+    .done(function (message) {
 
-//       var html = buildHTML(message);
-//       $('.chat-main__body--list').append(html)
+      var html = buildHTML(message);
+      $('.chat-main__body--list').append(html)
 
-//       $('#message-content').val('')
-//       $('.chat-main__body').animate({
-//         scrollTop: $('.chat-main__body')[0].scrollHeight
-//       }, 1000, 'swing');
-//     })
+      $('#message-content').val('')
+      $('.chat-main__body').animate({
+        scrollTop: $('.chat-main__body')[0].scrollHeight
+      }, 1000, 'swing');
+    })
 
-//     .fail(function () {
-//       alert('error');
-//     })
-//     .always(function () {
-//       $(".chat-main__footer--new-message--submit").removeAttr("disabled");
-//     });
-//   });
+    .fail(function () {
+      alert('error');
+    })
+    .always(function () {
+      $(".chat-main__footer--new-message--submit").removeAttr("disabled");
+    });
+  });
 
 
 
-//   $(function () {
-//     var interval = setInterval(function (){
-//       if (window.location.href.match(/\/groups\/\d+\/messages/)){
-//           var message_id = $('.chat-main__body--list--message').last().data('message-id');
-//           $.ajax({
-//             url: location.href,
-//             type: 'GET',
-//             data: {id: message_id},
-//             dataType: 'json'
-//           })
-//           .done(function(data){
-//             if (data.length !== 0 ){
-//               data.forEach(function(message){
-//                 var html = buildHTML(message);
-//                 $('.chat-main__body--list').append(html);
-//               });
+  $(function () {
+    var interval = setInterval(function (){
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+          var message_id = $('.chat-main__body--list--message').last().data('message-id');
+          $.ajax({
+            url: location.href,
+            type: 'GET',
+            data: {id: message_id},
+            dataType: 'json'
+          })
+          .done(function(data){
+            if (data.length !== 0 ){
+              data.forEach(function(message){
+                var html = buildHTML(message);
+                $('.chat-main__body--list').append(html);
+              });
 
-//               $('.chat-main__body').animate({
-//                 scrollTop: $('.chat-main__body')[0].scrollHeight
-//               }, 1000, 'swing');
-//             }
-//           })
-//           .fail(function() {
-//             alert('自動更新に失敗しました');
-//           });
-//          } else {
-//           clearInterval(interval);
-//       }}, 5000);
-//   });
-// });
+              $('.chat-main__body').animate({
+                scrollTop: $('.chat-main__body')[0].scrollHeight
+              }, 1000, 'swing');
+            }
+          })
+          .fail(function() {
+            alert('自動更新に失敗しました');
+          });
+         } else {
+          clearInterval(interval);
+      }}, 5000);
+  });
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,9 +14,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    logger.debug(@message.save)
-    logger.debug(@message.errors)
-    logger.debug(@message.errors.full_messages)
     if @message.save
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group) }
