1d7b06678a90f1be1d24e85deef6d0d9d40762de
Rick-32 <okano.0407@gmail.com>
2018-12-08 19:39:57 +0900

    edit/TimeZone, PicUploadData

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    # 連続投稿の送信ボタン無効を防ぐ
+    config.action_view.automatically_disable_submit_tag = false
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -15,5 +17,7 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
